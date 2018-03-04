6b9e28481ce8f0ebb7b6f7802c80e72e2d7e4e28
Alexander Turk <alexmyclo90@gmail.com>
2018-03-04 02:21:27 -0500

    wip2

--- src/project-3.js
+++ src/project-3.js
@@ -5,13 +5,23 @@ const makeCat = (name, age) => {
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
+  return {
+    name,
+    age,
+    meow: function () {
+      return `${this.name} meowed!`;
+    }
+  };
 };
 
 const addProperty = (object, property) => {
-  // add the property to the object with a value of null
+  object[property] = null;
+  return object;
+  
+addProperty({}, 'hello');// add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
-};
+
 
 const invokeMethod = (object, method) => {
   // method is a string that contains the name of a method on the object
@@ -20,6 +30,7 @@ const invokeMethod = (object, method) => {
 };
 
 const multiplyMysteryNumberByFive = (mysteryNumberObject) => {
+  return mysteryNumber.mysteryNumberObject * 5;
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
 };
